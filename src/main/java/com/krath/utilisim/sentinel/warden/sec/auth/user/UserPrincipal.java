cea887ef16ba4435007aa03a0278d718c11be9e1
Nicky Kayyarath <nkayyarath@gmail.com>
2026-07-25 00:57:14 -0400

    created user principal class for authentication when using controllers

--- src/main/java/com/krath/utilisim/sentinel/warden/sec/auth/user/UserPrincipal.java
+++ src/main/java/com/krath/utilisim/sentinel/warden/sec/auth/user/UserPrincipal.java
@@ -1,4 +1,13 @@
 package com.krath.utilisim.sentinel.warden.sec.auth.user;
 
+import lombok.Getter;
+
+@Getter
 public class UserPrincipal {
+    private final String id;
+
+    public UserPrincipal(String id) {
+        this.id = id;
+    }
+
 }
