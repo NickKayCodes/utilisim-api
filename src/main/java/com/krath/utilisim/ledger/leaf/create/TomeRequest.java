9f70708ce7b874f6a7efa649987de6149e5152e4
Nicky Kayyarath <nkayyarath@gmail.com>
2026-07-29 23:24:57 -0400

    found typo in TomeRequest.java, fixed the typo.

--- src/main/java/com/krath/utilisim/ledger/leaf/create/TomeRequest.java
+++ src/main/java/com/krath/utilisim/ledger/leaf/create/TomeRequest.java
@@ -3,7 +3,7 @@ package com.krath.utilisim.ledger.leaf.create;
 public record TomeRequest(
         String name,
         float currentBalance,
-        Float apr,               // nullable
+        float apr,               // nullable
         float minimumDebtPayment,
         float creditLimit,
         String type
