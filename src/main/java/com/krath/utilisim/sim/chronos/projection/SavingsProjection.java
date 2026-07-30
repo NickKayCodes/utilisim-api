5757405031694299c2431f93e681c94e26bdab69
Nicky Kayyarath <nkayyarath@gmail.com>
2026-07-29 23:38:02 -0400

    add SavingsProjection model for baseline vs scenario interest comparison

---
+++ src/main/java/com/krath/utilisim/sim/chronos/projection/SavingsProjection.java
@@ -0,0 +1,14 @@
+package com.krath.utilisim.sim.chronos.projection;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SavingsProjection {
+
+    private float baselineInterest;
+    private float scenarioInterest;
+    private float savings;
+
+}
