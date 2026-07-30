5022f9976ada1f0fc1681a2a21a9193ec5afcc77
Nicky Kayyarath <nkayyarath@gmail.com>
2026-07-29 23:48:06 -0400

    apr can be null, the primitive data type of float does not allow nullable data sent from front end. updated the data type to a non-primitive

--- src/main/java/com/krath/utilisim/ledger/leaf/create/TomeRequest.java
+++ src/main/java/com/krath/utilisim/ledger/leaf/create/TomeRequest.java
@@ -3,7 +3,7 @@ package com.krath.utilisim.ledger.leaf.create;
 public record TomeRequest(
         String name,
         float currentBalance,
-        float apr,               // nullable
+        Float apr,               // nullable
         float minimumDebtPayment,
         float creditLimit,
         String type
